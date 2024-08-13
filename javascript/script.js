cf5b0ac78ea3db8b6cb4b015e1c1735185161ac5
Paula Fenner <fenner.paula@gmail.com>
2024-08-13 21:22:09 +0200

    Styled the order container
    Added html boilerplate text.

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,12 @@
-import { menuArray } from './javascript/data.js';
+import { menuArray } from './data.js';
 
 const menuContainerEl = document.querySelector('.menu-container');
+const orderContainerEl = document.querySelector('.order-container');
 
 function populateMenuContainer() {
   for (let menuItem of menuArray) {
     generateMenuHtml(menuItem);
+    // generateOrderHtml(menuItem);
   }
 }
 
@@ -14,12 +16,30 @@ function generateMenuHtml(menuItem) {
         <span class="item-image" >${menuItem.emoji}</span>
         <div class="item-description">
             <h2>${menuItem.name}</h2>
-            <p>${menuItem.ingredients}</p>
-            <p>$ ${menuItem.price}</p>
+            <p class="item-ingredients">${menuItem.ingredients}</p>
+            <p class="price">€ ${menuItem.price}</p>
         </div>
         <button class="item-add-btn">+</button>
     </section>
       `;
 }
+
+// function generateOrderHtml(menuItem) {
+//   orderContainerEl.innerHTML += `
+//     <h3>Your Order</h3>
+//     <ul class="order">
+//         <li class="order-item">
+//             <p>${menuItem.name}</p>
+//             <button class="remove-btn" id="remove-btn">remove</button>
+//             <p><span>€</span> ${menuItem.price}</p>
+//         </li>
+//     </ul>
+//     <div class="order">
+//         <p>Total to pay:</p>
+//         <p>€xx</p>
+//     </div>
+//     <button>Complete order</button>
+//     `;
+// }
 
 populateMenuContainer();
