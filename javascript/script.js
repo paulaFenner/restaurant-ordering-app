56d9f455b164e15cbf530b2a2d2c2c72dd793a35
Paula Fenner <fenner.paula@gmail.com>
2024-08-17 21:22:39 +0200

    Implemented basic functionality on checkout, payment and delivery operations.

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,25 @@
 import { menuArray } from './data.js';
 
+let order = [];
+let orderSum = 0;
+
 const menuContainerEl = document.querySelector('.menu-container');
 const orderContainerEl = document.querySelector('.order-container');
+const completeOrderBtn = document.getElementById('complete-order-btn');
+const modalPaymentEl = document.getElementById('modal');
+const modalPayBtn = document.querySelector('.modal-pay-btn');
+
+menuContainerEl.addEventListener('click', handleItemSelected);
+completeOrderBtn.addEventListener('click', handleCheckout);
+modalPayBtn.addEventListener('click', orderCompleted);
 
 function populateMenuContainer() {
   for (let menuItem of menuArray) {
-    generateMenuHtml(menuItem);
-    // generateOrderHtml(menuItem);
+    generateMenuList(menuItem);
   }
 }
 
-function generateMenuHtml(menuItem) {
+function generateMenuList(menuItem) {
   menuContainerEl.innerHTML += `
     <section class="menu-item">
         <span class="item-image" >${menuItem.emoji}</span>
@@ -19,27 +28,81 @@ function generateMenuHtml(menuItem) {
             <p class="item-ingredients">${menuItem.ingredients}</p>
             <p class="price">€ ${menuItem.price}</p>
         </div>
-        <button class="item-add-btn">+</button>
+        <button class="item-add-btn" data-id="${menuItem.id}">+</button>
     </section>
       `;
 }
 
-// function generateOrderHtml(menuItem) {
-//   orderContainerEl.innerHTML += `
-//     <h3>Your Order</h3>
-//     <ul class="order">
-//         <li class="order-item">
-//             <p>${menuItem.name}</p>
-//             <button class="remove-btn" id="remove-btn">remove</button>
-//             <p><span>€</span> ${menuItem.price}</p>
-//         </li>
-//     </ul>
-//     <div class="order">
-//         <p>Total to pay:</p>
-//         <p>€xx</p>
-//     </div>
-//     <button>Complete order</button>
-//     `;
+function handleItemSelected(e) {
+  if (e.target.classList.contains('item-add-btn')) {
+    appendOrder(e);
+  }
+}
+
+function appendOrder(e) {
+  const productId = e.target.dataset.id;
+  const product = menuArray.filter((item) => item.id === parseInt(productId))[0];
+  if (product) {
+    order.push(product);
+    orderContainerEl.style.display = 'block';
+    generateOrderList(product);
+  }
+}
+
+function generateOrderList(product) {
+  document.querySelector('.order-selection').innerHTML += `
+    <li class="order-item">
+        <h4>${product.name}</h4>
+        <button class="remove-btn" id="remove-btn">remove</button>
+        <p class="price"><span>€</span>${product.price}</p>
+    </li>
+     `;
+
+  generateTotalPrice(product);
+}
+
+function generateTotalPrice(product) {
+  orderSum += product.price;
+  document.querySelector('.order-total').innerHTML = `
+    <h4>Total price:</h4>
+    <p class="price">€${orderSum}</p>
+     `;
+}
+
+// function removeItemFromOrder() {
+//   document.getElementById('remove-btn').addEventListener('click', function () {});
 // }
+
+function handleCheckout() {
+  modalPaymentEl.style.display = 'block';
+}
+
+function getFirstNameCardholder() {
+  const fullNameInput = document.querySelector('[name="cardholder-name"]');
+  return fullNameInput.value.split(' ')[0];
+}
+
+function generateCompletionMessage() {
+  orderContainerEl.innerHTML = `
+  <h3 class="dispatched-message">Thanks, ${getFirstNameCardholder()}!
+  <br> Your order is on its way!</h3>
+  <button class="new-order-btn" id="new-order-btn">New Order</button>
+  `;
+}
+
+function clearPaymentInput() {
+  document.querySelector('[name="cardholder-name"]').value = '';
+  document.querySelector('[name="card-number"]').value = '';
+  document.querySelector('[name="expiration-date"]').value = '';
+  document.querySelector('[name="cvv-cvc"]').value = '';
+}
+
+function orderCompleted() {
+  order = [];
+  orderSum = 0;
+  modalPaymentEl.style.display = 'none';
+  generateCompletionMessage();
+  clearPaymentInput();
+}
 
 populateMenuContainer();
---
+++ steps.md
@@ -0,0 +1,51 @@
+# Steps on restaurant ordering app
+
+1. set up the header
+
+- [x] finding the image
+
+- [x] importing the font
+- [x] create the content
+- [x] style with css
+
+1. start by setting up one menu item
+
+- [x] icon to the left
+- [x] text
+- [x] plus to right
+- [x] style
+- [x] remove the content
+
+3. rendering the menu items from the provided array
+
+- [x] loop through all items
+- [x] generate html
+
+4. set up checkout box
+
+- [x] header
+- [x] put in content (pizza remove and price)
+- [x] put in total price
+- [x] put in button
+- [x] style that
+- [x] remove the price and menu item
+
+5. making on click work for plus button
+
+- [x] you'll need a way to find which item was added
+
+6. - [x] add render item(s) in checkout box
+7. - [x] hide the checkout box when nothing is in there (if all items are removed)
+8. - [x] When complete order button is clicked, open modal
+
+- [x] header, text fields, and a pay button
+
+9. when pay button is clicked
+
+- change checkout box for notification
+  - close modal
+
+1.  added a new order button
+
+- below the notification
+- button refreshed the page
